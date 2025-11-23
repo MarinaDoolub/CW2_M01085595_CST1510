425cc323dda2be99a056b7c8e4702ed197de8b3d
Marina <doolubmarina15@gmail.com>
2025-11-23 18:33:35 +0400

    creation of users.py and creating function to retrieve user by username

--- app/data/users.py
+++ app/data/users.py
@@ -1,10 +1,10 @@
 from app.data.db import connect_database
+import bcrypt
 
 def get_user_by_username(username):
-    """Retrieve user by username."""
+    #Retrieve user by username
     conn = connect_database()
     cursor = conn.cursor()
-    
     cursor.execute(
         "SELECT * FROM users WHERE username = ?",
         (username,)
@@ -12,14 +12,3 @@ def get_user_by_username(username):
     user = cursor.fetchone()
     conn.close()
     return user
-
-def insert_user(username, password_hash, role='user'):
-    """Insert new user."""
-    conn = connect_database()
-    cursor = conn.cursor()
-    cursor.execute(
-        "INSERT INTO users (username, password_hash, role) VALUES (?, ?, ?)",
-        (username, password_hash, role)
-    )
-    conn.commit()
-    conn.close()
