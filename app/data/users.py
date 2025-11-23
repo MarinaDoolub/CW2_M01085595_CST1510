3a27f5692036cc915d853191cc738aec2dc16e3c
Marina <doolubmarina15@gmail.com>
2025-11-23 18:35:27 +0400

    creating function to insert new user

--- app/data/users.py
+++ app/data/users.py
@@ -12,3 +12,27 @@ def get_user_by_username(username):
     user = cursor.fetchone()
     conn.close()
     return user
+
+#------------------------------------------------------------------------
+#create new user
+def insert_user(username, plain_password, role='user'):
+
+    # Hash the password
+    password_bytes = plain_password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    password_hashed = bcrypt.hashpw(password_bytes, salt).decode('utf-8')
+
+    # Connect to database and insert user
+    conn = connect_database()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO users (username, password_hash, role) VALUES (?, ?, ?)",
+        (username, password_hashed, role)
+    )
+    conn.commit()
+
+    #this is to return the inserted user ID
+    last_id = cursor.lastrowid
+    conn.close()
+
+    return last_id
